0567e589693f2ab849beb577ac5621a21255a326
NTsuchiya0127 <angelbeatskanade61st@yahoo.co.jp>
2017-05-15 08:27:15 +0900

    Update MyInterpolations.jl

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -1,10 +1,18 @@
 module MyInterpolations
 function my_lin_interp(grid, vals)
     function func(x)
+        if x < grid[1]
+            print("error")
+        end
+        if x > grid[end]
+            print("error")
+        end
+        if grid[1] <= x <= grid[end]
         i = searchsortedlast(grid, x)
         x in grid[i]:grid[i+1]
         return vals[i] + (vals[i+1] - vals[i])*(x - grid[i])/(grid[i+1] - grid[i])
     end
     return func
 end
+    export my_lin_interp
 end
