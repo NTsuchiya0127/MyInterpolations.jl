2227d90ab44d7b67a00b4a21097ac0a31c130dd3
NTsuchiya0127 <angelbeatskanade61st@yahoo.co.jp>
2017-05-15 09:02:31 +0900

    Update MyInterpolations.jl

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -12,6 +12,7 @@ function my_lin_interp(grid, vals)
         x in grid[i]:grid[i+1]
         return vals[i] + (vals[i+1] - vals[i])*(x - grid[i])/(grid[i+1] - grid[i])
     end
+    end
     return func
 end
 export my_lin_interp
